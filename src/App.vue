a0760a87a8d8f346cea03ccbe4a76b50d4464760
xuyunfei <987517825@qq.com>
2023-05-30 22:26:13 +0800

    '提交'

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="box">app</div>
+</template>
+<script setup lang="ts"></script>
+<style scoped lang="scss">
+.box {
+  width: 100px;
+  height: 100px;
+  background: red;
+}
+</style>
---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+const speak = () => {
+
+    console.log("咕咕咕")
+}
+createApp(App).mount('#app')
