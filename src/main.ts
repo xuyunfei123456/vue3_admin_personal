6576116507121e4170b684e42ff250a89b133c22
xuyunfei <987517825@qq.com>
2023-05-30 22:55:32 +0800

    feat: 测试提交代码前检验代码规范

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 const speak = () => {
-  console.log('咕咕咕')
+  const a = 123
+     console.log('咕咕咕', a)
 }
+console.log(speak())
 createApp(App).mount('#app')
